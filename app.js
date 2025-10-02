2d04db7f4fea1afb5e947e7e949b1cfeeaeaf588
Mohini Ruhela <ruhelamohini@gmail.com>
2025-10-02 17:01:35 +0530

    Final submit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Mobile Navigation Toggle
+// For Mobile Navigation Toggle
         const hamburger = document.querySelector('.hamburger');
         const navLinks = document.querySelector('.nav-links');
         
